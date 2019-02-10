cac0988fa670c7d35f84e879e863fa94c0a97e42
Jabba Laci <jabba.laci@gmail.com>
2019-02-10 11:25:10 +0100

    I/O operations (read/write file)

--- hello.py
+++ hello.py
@@ -6,10 +6,14 @@ including 3rd-party libraries
 """
 
 import os
+import sys
+from pathlib import Path
 
 import requests
 
 import helper
+
+BASE_DIR = os.path.dirname(os.path.abspath(sys.argv[0]))
 
 
 def main():
@@ -20,6 +24,17 @@ def main():
     print("PATH:", os.getenv("PATH"))
     result = helper.add(1, 1)
     print(f"1 + 1 = {result}")
+    try:
+        with open(Path(BASE_DIR, "input.txt")) as f:
+            content = f.read().strip()
+        print(content)
+    except IOError:
+        print("Warning: input.txt was not found")
+    #
+    fname = Path(BASE_DIR, "output.txt")
+    print("writing to the following file:", fname)
+    with open(fname, "w") as g:
+        print("writing to a file", file=g)
 
 ##############################################################################
 
